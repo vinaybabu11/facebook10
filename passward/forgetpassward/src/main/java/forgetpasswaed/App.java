2d35ae371ddcd929d6b3b2dc5c35c74e673801e8
unknown <kantha>
2022-04-26 18:24:17 +0530

    devops working for facebook

---
+++ passward/forgetpassward/src/main/java/forgetpasswaed/App.java
@@ -0,0 +1,13 @@
+package forgetpasswaed;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
